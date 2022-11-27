22334884d65d00fc2bbc9974970fb38589aaef5e
SlimeVRX <99313947+SlimeVRX@users.noreply.github.com>
2022-11-27 12:19:25 +0700

    Update 1-4.py

--- ch01_code/1-4.py
+++ ch01_code/1-4.py
@@ -1,3 +1,12 @@
-bpy.ops.mesh.primitive_cube_add(radius=1, view_align=False, enter_editmode=False,
-                                location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False,
-                                                            False, False, False, False, False, False, False, False, False, False, False, False))
+# bpy.ops.mesh.primitive_cube_add(radius=1, view_align=False, enter_editmode=False,
+#                                 location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False,
+#                                                             False, False, False, False, False, False, False, False, False, False, False, False))
+
+bpy.ops.mesh.primitive_cube_add(size=2.0, 
+                                calc_uvs=True, 
+                                enter_editmode=False, 
+                                align='WORLD', 
+                                location=(0.0, 0.0, 0.0), 
+                                rotation=(0.0, 0.0, 0.0), 
+                                scale=(0.0, 0.0, 0.0)
+                               )
